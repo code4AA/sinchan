d6902c233c0ec04128f3d213939e7fcbb98ecf16
code4AA <62166340+code4AA@users.noreply.github.com>
2021-06-10 09:30:38 +0530

    Add files via upload

---
+++ vm projects/js/global.js
@@ -0,0 +1,13 @@
+var age1=30;
+console.log(age1)
+function firstFunction(){
+ age=10;
+console.log(age)
+}
+function secondFunction(){
+var name="akash";
+console.log(name)
+console.log(age)
+}
+firstFunction()
+secondFunction()
